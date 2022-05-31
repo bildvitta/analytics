dd56554e7615020166c77b95cadcce700c383e41
Douglas Calora <douglascalora123@gmail.com>
2022-05-31 19:21:49 -0300

    fix grammar

--- src/templates/analytics.conf.js
+++ src/templates/analytics.conf.js
@@ -2,7 +2,7 @@
  * Se estiver utilizando esta extensão com quasar v2:
  * https://github.com/gtm-support/vue-gtm/tree/main
  *
- * Caso esteja esta extensão com quasar v1:
+ * Caso esteja utilizando esta extensão com quasar v1:
  * https://github.com/gtm-support/vue-gtm/tree/vue2-gtm
  *
  * OBS: A configuração do "vueRouter: router" já é feita automaticamente pela extensão.
