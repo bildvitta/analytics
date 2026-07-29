520dede7875b848d43788aae67b635a26cc564ee
felipejuzo02 <felipejuzo@hotmail.com>
2026-07-29 17:09:33 -0300

    fix gtm boot

--- src/boot/gtm.js
+++ src/boot/gtm.js
@@ -1,20 +1,16 @@
 import { useGtm } from '../composables'
-
 import { createGtm } from '@gtm-support/vue-gtm'
 
 export default ({ router, app }) => {
-  const id = process.env.ANALYTICS_KEY
+  const id = import.meta.env?.VITE_ANALYTICS_KEY || import.meta.env?.ANALYTICS_KEY
 
   if (!id) return
 
   try {
     const { setGtmInstance } = useGtm()
-    const hasDebug = process.env.DEBUGGING || false
+    const hasDebug = Boolean(import.meta.env?.VITE_DEBUGGING || import.meta.env?.DEBUGGING)
 
-    app.use(
-      createGtm({ id, debug: hasDebug, vueRouter: router })
-    )
-
+    app.use(createGtm({ id, debug: hasDebug, vueRouter: router }))
     setGtmInstance(app.config.globalProperties.$gtm)
   } catch {
     console.error('Erro ao inicializar VueGtm, "@gtm-support/vue-gtm"')
