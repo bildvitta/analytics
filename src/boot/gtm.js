700cc7157776b3d907f4660f71070e7edb0aee26
felipejuzo02 <felipejuzo@hotmail.com>
2026-07-30 12:09:07 -0300

    fix envs

--- src/boot/gtm.js
+++ src/boot/gtm.js
@@ -2,13 +2,13 @@ import { useGtm } from '../composables'
 import { createGtm } from '@gtm-support/vue-gtm'
 
 export default ({ router, app }) => {
-  const id = import.meta.env?.VITE_ANALYTICS_KEY || import.meta.env?.ANALYTICS_KEY
+  const id = import.meta.env?.ANALYTICS_KEY
 
   if (!id) return
 
   try {
     const { setGtmInstance } = useGtm()
-    const hasDebug = Boolean(import.meta.env?.VITE_DEBUGGING || import.meta.env?.DEBUGGING)
+    const hasDebug = Boolean(import.meta.env?.DEBUGGING)
 
     app.use(createGtm({ id, debug: hasDebug, vueRouter: router }))
     setGtmInstance(app.config.globalProperties.$gtm)
